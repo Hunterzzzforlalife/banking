55497cf442a6110ceea1152a4dcf311709030882
Hunterzzzforlalife <30748172+Hunterzzzforlalife@users.noreply.github.com>
2017-08-05 12:21:21 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,20 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+ui_page 'html/ui.html'
+files {
+	'html/ui.html',
+	'html/pricedown.ttf',
+	'html/bank-icon.png',
+	'html/logo.png',
+	'html/cursor.png',
+	'html/styles.css',
+	'html/scripts.js',
+	'html/debounce.min.js',
+	'html/background.png',
+	'html/background-home.png',
+	'html/desjardins.png',
+	'html/background-confirm.png'
+}
+
+client_script "client.lua"
+server_script "server.lua"
